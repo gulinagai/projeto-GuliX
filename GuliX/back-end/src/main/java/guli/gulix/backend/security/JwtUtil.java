aeaf2fa89f9612164ddbc69b90fb476867302604
Gustavo Nagai Amorim <gulifiree@gmail.com>
2026-05-04 00:38:29 -0300

    implrefatorando Jwtutil em impltação spring security auautenticação, access token, gerartoken, validartoken, exceptions

--- GuliX/back-end/src/main/java/guli/gulix/backend/security/JwtUtil.java
+++ GuliX/back-end/src/main/java/guli/gulix/backend/security/JwtUtil.java
@@ -1,11 +1,13 @@
 package guli.gulix.backend.security;
 
 import guli.gulix.backend.entity.Usuario;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -17,30 +19,45 @@ public class JwtUtil {
     @Value("${jwt.expiration}")
     private long expiration;
 
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
     public String gerarToken(Usuario usuario) {
         return Jwts.builder()
                 .setSubject(usuario.getEmail())
                 .claim("id", usuario.getId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expiration)) // expiration é 1h
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String extrairEmail(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+        return extrairClaims(token).getSubject();
+    }
+
+    public Claims extrairClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody();
     }
 
     public boolean validarToken(String token) {
         try {
-            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            extrairClaims(token);
             return true;
-        } catch (Exception e) {
-            return false;
+        } catch (ExpiredJwtException e) {
+            System.out.println("Token expirado");
+        } catch (UnsupportedJwtException e) {
+            System.out.println("Token não suportado");
+        } catch (MalformedJwtException e) {
+            System.out.println("Token malformado");
+        } catch (JwtException | IllegalArgumentException e) {
+            System.out.println("Token inválido");
         }
+        return false;
     }
 }
