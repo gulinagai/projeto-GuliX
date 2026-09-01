9d2f7eff01f144177638cce9c0269a87c0d87874
Gustavo Nagai Amorim <gulifiree@gmail.com>
2026-09-01 19:56:45 -0300

    bugfix(estoque): trata produtoId em EstoqueResponseDTO configurando source e target no EstoqueMapper

--- GuliX/backend/src/main/java/guli/gulix/backend/mapper/EstoqueMapper.java
+++ GuliX/backend/src/main/java/guli/gulix/backend/mapper/EstoqueMapper.java
@@ -4,6 +4,7 @@ import guli.gulix.backend.dto.EstoqueRequestDTO;
 import guli.gulix.backend.dto.EstoqueResponseDTO;
 import guli.gulix.backend.entity.Estoque;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -11,7 +12,7 @@ public interface EstoqueMapper {
 
     void updateFromDto(EstoqueRequestDTO dto, @MappingTarget Estoque estoque);
 
-
+    @Mapping(source = "produto.id", target = "produtoId")
     EstoqueResponseDTO toDTO(Estoque estoque);
 
 }
